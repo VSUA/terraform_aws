40009b37a25df3b0c467b000044260aacbb55968
Vladyslav Slobodianiuk <vs150502@gmail.com>
2021-08-18 15:41:47 +0300

    Add route for private route table to NAT gw

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -70,6 +70,10 @@ resource "aws_route_table_association" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.nginx_vpc.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
   tags = {
     Name = "private-rt"
   }
