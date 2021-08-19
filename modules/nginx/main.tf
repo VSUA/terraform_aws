0f4a3e204e2e71102114d63ade97db355fffb513
Vladyslav Slobodianiuk <vs150502@gmail.com>
2021-08-19 18:57:03 +0300

    Add usage of nginx module

--- modules/nginx/main.tf
+++ modules/nginx/main.tf
@@ -2,24 +2,21 @@ resource "aws_security_group" "http_sg" {
   name        = "http_sg"
   vpc_id      = var.aws_vpc_id
 
-  ingress = [
-    {
+  ingress = {
       from_port        = 80
       to_port          = 80
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
     }
-  ]
 
-  egress = [
-    {
+
+  egress = {
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
     }
-  ]
 }
 
 
