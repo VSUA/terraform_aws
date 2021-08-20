dfe95b39af16c5c81b4865f3eae02a57bf3de7bc
Vladyslav Slobodianiuk <vs150502@gmail.com>
2021-08-20 15:48:03 +0300

    Add credentials for backend

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ terraform {
     key = "global/s3/terraform.tfstate"
     region = "eu-central-1"
     dynamodb_table = "terraformstate"
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
   }
 }
 
